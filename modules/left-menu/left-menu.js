a43d02bcaac87d71aee43eeb3ecd27323f786b5a
PebPeb <bmkeen@liberty.edu>
2026-01-14 16:19:36 -0500

    Added being able to navigate for mobile users/smaller screens

--- modules/left-menu/left-menu.js
+++ modules/left-menu/left-menu.js
@@ -6,13 +6,20 @@ fetch("/modules/left-menu/left-menu.html")
     return response.text();
   })
   .then(data => {
-    const nav = document.querySelector(".left-menu");
+    const nav = document.querySelector(".left-menu-container");
     if (!nav) {
       console.error("No element with class 'left-menu' found.");
       return;
     }
     nav.innerHTML = data;
     console.log("left-menu inserted successfully.");
+
+    const btn = document.getElementById("mobile-menu-btn");
+    btn.addEventListener("click", () => {
+      const leftMenu = document.querySelector(".left-menu");
+      leftMenu.classList.toggle("show");  
+    });
+
   })
   .catch(error => {
     console.error("Fetch failed:", error);
